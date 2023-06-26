5db2e4a6e8e0db25a99aff453a4ef9b53d19530b
Ha Ho <ha.ho@Has-MacBook-Pro.local>
2023-06-26 22:08:58 +0700

    refactor: Refactor the unit tests

--- repositories/pokemon_test.go
+++ repositories/pokemon_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/hoducha/ondemand-go-bootcamp/models"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCSVRepository_GetByID(t *testing.T) {
@@ -20,28 +21,17 @@ func TestCSVRepository_GetByID(t *testing.T) {
 
 	// Test case: Existing Pokemon
 	pokemon, err := repo.GetByID(1)
-	if err != nil {
-		t.Errorf("Expected no error, got: %v", err)
-	}
-	if pokemon == nil {
-		t.Error("Expected Pokemon, got nil")
-	} else if pokemon.ID != 1 || pokemon.Name != "Bulbasaur" {
-		t.Errorf("Expected Pokemon with ID 1 and name 'Bulbasaur', got: %+v", pokemon)
-	}
+	assert.NoError(t, err)
+	assert.NotNil(t, pokemon)
+	assert.Equal(t, 1, pokemon.ID)
+	assert.Equal(t, "Bulbasaur", pokemon.Name)
 
 	// Test case: Non-existing Pokemon
 	pokemon, err = repo.GetByID(3)
-	if err == nil {
-		t.Error("Expected error, got nil")
-	}
-	if pokemon != nil {
-		t.Errorf("Expected nil Pokemon, got: %+v", pokemon)
-	}
+	assert.Error(t, err)
+	assert.Nil(t, pokemon)
 	expectError := errors.New("Pokemon not found")
-	if err.Error() != expectError.Error() {
-		t.Errorf("Expected '%v', got: %v", expectError, err)
-	}
-
+	assert.EqualError(t, err, expectError.Error())
 }
 
 func TestNewPokemonRepository(t *testing.T) {
@@ -52,18 +42,12 @@ func TestNewPokemonRepository(t *testing.T) {
 
 	// Test case: Valid CSV file
 	repo, err := NewPokemonRepositoryFromReader(r)
-	if err != nil {
-		t.Errorf("Expected no error, got: %v", err)
-	}
-	if repo == nil {
-		t.Error("Expected repository instance, got nil")
-	}
+	assert.NoError(t, err)
+	assert.NotNil(t, repo)
 
 	// Test case: Invalid CSV file
 	invalidCSV := "invalid csv data"
 	r = strings.NewReader(invalidCSV)
 	_, err = NewPokemonRepositoryFromReader(r)
-	if err == nil {
-		t.Error("Expected error, got nil")
-	}
+	assert.Error(t, err)
 }
